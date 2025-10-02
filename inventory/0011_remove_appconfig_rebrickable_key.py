dfbcb4294a8455b818acdee8c31c3305f9aa5150
DarkSmileee <60740373+DarkSmileee@users.noreply.github.com>
2025-10-02 15:06:40 +0200

    Create 0011_remove_appconfig_rebrickable_key.py

---
+++ inventory/0011_remove_appconfig_rebrickable_key.py
@@ -0,0 +1,14 @@
+# Generated migration to remove rebrickable_api_key from AppConfig
+from django.db import migrations
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inventory', '0010_add_data_integrity_constraints'),
+    ]
+
+  operations = [
+        migrations.RemoveField(
+            model_name='appconfig',
+            name='rebrickable_api_key',
+        ),
+    ]
